749930c1f64f857eb43424a880772f2ce91216db
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-04-16 17:37:25 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galeryEl = document.querySelector(".gallery");
+const createGalery = getImgOriginal(galleryItems);
+galeryEl.insertAdjacentHTML("beforeend", createGalery);
+
+function getImgOriginal(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+               class="gallery__image"
+               src="${preview}"
+               data-source="${original}"
+               alt="${description}"
+            />
+          </a>
+        </li>`;
+    })
+    .join("");
+}
